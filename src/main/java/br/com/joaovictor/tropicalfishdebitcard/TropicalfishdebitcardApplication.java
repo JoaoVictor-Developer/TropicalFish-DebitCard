4f7347c193846c44c810d16b5aee45a662d105c5
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-09-22 15:33:01 -0300

    I started the project, with basic configurations done and connected to git hub

---
+++ src/main/java/br/com/joaovictor/tropicalfishdebitcard/TropicalfishdebitcardApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaovictor.tropicalfishdebitcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TropicalfishdebitcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TropicalfishdebitcardApplication.class, args);
+	}
+
+}
